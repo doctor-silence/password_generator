4df47af8676bb514ec9a0ef9e048d47859368e4d
doctor-silence <vladimirkrizhanovski@gmail.com>
2022-09-17 13:13:48 +0700

    add_new_views

--- password_app/views.py
+++ password_app/views.py
@@ -1,5 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import random
 
 def home(request):
-    return HttpResponse('hello')
+    return render(request, 'generator/home.html')
+
+def password(request):
+
+    characters = list('abcdefghijklmnopqstuvwxyz')
+
+    length = 10
+
+    thepassword = ''
+    for x in range(length):
+        thepassword += random.choice(characters)
+
+    return render(request, 'generator/password.html', {'password': thepassword})
