299732bb5e8c72c54902f9d4b560bf5d3c2bcb7c
doctor-silence <vladimirkrizhanovski@gmail.com>
2022-09-20 15:03:02 +0700

    addNewView

--- password_app/views.py
+++ password_app/views.py
@@ -5,11 +5,21 @@ import random
 def home(request):
     return render(request, 'generator/home.html')
 
+def info(request):
+    return render(request, 'generator/info.html')
+
 def password(request):
 
     characters = list('abcdefghijklmnopqstuvwxyz')
 
-    length = 10
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQSTUVWXYZ'))
+    if request.GET.get('special'):
+            characters.extend(list('!@#$%^&*(){}"|:><?'))
+    if request.GET.get('numbers'):
+        characters.extend(list('0123456789'))
+
+    length = int(request.GET.get('length', 12))
 
     thepassword = ''
     for x in range(length):
